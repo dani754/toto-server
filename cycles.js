32171d5e251058fe0b9cc1f546aeb05482f3c941
Danielle Vaitsman <d734051@gmail.com>
2021-08-12 16:49:58 +0300

    cycle table data

--- cycles.js
+++ cycles.js
@@ -2,6 +2,7 @@ const knex = require('knex');
 const DBinfo = require('./DB_info');
 const dataBase = knex(DBinfo.get());
 
+const games = require('./games');
 const table = 'cycles_1';
 
 const getCycleScores = (req,res) => {
@@ -15,5 +16,21 @@ const getCycleScores = (req,res) => {
     }).catch(err => {return err});
 }
 
+const getCycleData = (req,res) => {
+    let data = {};
+    return dataBase.select('*').from(table)
+    .where('cycleid','=',req.params.id).returning('*')
+    .then( answer => {
+        data = answer[0];
+        return games.getGamesDB(req.params.id);
+    }).then ( answer1 => {
+        data.gamesDB = answer1;
+        console.log("the cycle and games info is: ", data);
+        res.send(data);
+        res.end();        
+    }).catch(err => {return err});
+}
+
 exports.getCycleScores = getCycleScores;
+exports.getCycleData = getCycleData;
 
--- games.js
+++ games.js
@@ -4,3 +4,11 @@ const dataBase = knex(DBinfo.get());
 
 const table = 'games_1';
 
+const getGamesDB = (cycleID) => {
+    return dataBase.select('*').from(table)
+    .where('cycleid','=',cycleID).returning('*')
+    .catch(err => {return err});
+}
+
+exports.getGamesDB = getGamesDB;
+
